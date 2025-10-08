0ec604ec502957171cf29fcc27575a5109a3feb3
Vedika Udgir <vedikaudgir@gmail.com>
2025-10-08 23:59:09 +0530

    que 5

---
+++ Lab5/problem4/knapsackAlgorithm.c
@@ -0,0 +1,11 @@
+// Implementing knapsack algorithm.
+// Author - Vedika Udgir
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
+int value[MAX];
+int weight[MAX];
+int ratio[MAX];
+int sackCapacity;
